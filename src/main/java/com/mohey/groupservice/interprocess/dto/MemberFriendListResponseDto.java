0f3702670f312189b1ae37820f77af617a887094
genboyjy <genboyjy@gmail.com>
2023-08-15 11:49:11 +0900

    :bug: fix: MemberFriendListResponseDto 수정

--- src/main/java/com/mohey/groupservice/interprocess/dto/MemberFriendListResponseDto.java
+++ src/main/java/com/mohey/groupservice/interprocess/dto/MemberFriendListResponseDto.java
@@ -1,7 +1,9 @@
 package com.mohey.groupservice.interprocess.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 
@@ -9,13 +11,25 @@ import lombok.Getter;
 public class MemberFriendListResponseDto {
 	private Integer code;
 	private String msg;
-	Data data;
+	List<Data> data;
 
 	public List<String> getFriendList(){
-		return data.friendList;
+		List<String> friendsUuidList = new ArrayList<>();
+		for (Data datum : data) {
+			friendsUuidList.add(datum.memberUuid);
+		}
+
+		return friendsUuidList;
 	}
 
+	@lombok.Data
+	@AllArgsConstructor
 	static class Data {
-		List<String> friendList;
+		private String memberUuid;
+		private String nickname;
+		private GenderEnum gender;
+		private String birthDate;
+		private String profileUrl;
+		private Boolean favoriteStatus;
 	}
 }
